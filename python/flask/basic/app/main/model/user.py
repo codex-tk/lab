ad560ef598aa6374b37fcbf8eefe8e83379ebca7
tlab <gwonhyeong.tak@gmail.com>
2020-05-28 23:37:51 +0900

    [title] add user password hash feature

--- python/flask/basic/app/main/model/user.py
+++ python/flask/basic/app/main/model/user.py
@@ -13,5 +13,16 @@ class User(db.Model):
     username = db.Column(db.String(50), unique=True)
     password_hash = db.Column(db.String(100))
 
+    @property
+    def password(self):
+        raise AttributeError('password: write only field')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password)
+
     def __repr__(self):
         return "<User '{}'>".format(self.username)
