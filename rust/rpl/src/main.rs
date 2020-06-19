2d55e03b810e23882f471275cb0ea9291e11dfd9
GHT <gwonhyeong.tak@gmail.com>
2020-06-19 14:58:10 +0900

    [title] rust rpl

--- rust/rpl/src/main.rs
+++ rust/rpl/src/main.rs
@@ -1,5 +1,8 @@
+mod variables;
 mod guess_game;
 
 fn main() {
-    guess_game::guess_game();
+    //guess_game::guess_game();
+    variables::variables();
+    println!("{}", variables::MAX_POINTS);
 }
---
+++ rust/rpl/src/variables.rs
@@ -0,0 +1,8 @@
+pub const MAX_POINTS : u32 = 100_000;
+
+pub fn variables(){
+    let mut x = 5;
+    println!("The value x is {}" , x);
+    x = 6;
+    println!("The value x is {}" , x);
+}
